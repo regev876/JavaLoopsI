8305e27589e9370a121b895709fd3874e0b1a890
regev <regev876@gmail.com>
2020-04-16 21:46:51 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            int result = (N * i);
+            System.out.printf("%d * %d = %d%n",N,i,result);
+        }
+    }
+}
